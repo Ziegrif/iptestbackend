8fac95defeff498b00728e0afcc3192d7856f67d
Petteri Hellman <petteri.hellman@gmail.com>
2023-02-20 14:21:55 +0200

    2022023

--- index.js
+++ index.js
@@ -69,6 +69,46 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+    .then(notes => {
+      if (notes) {
+        response.json(notes)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

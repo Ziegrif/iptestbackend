00544143ea4de8785252e53c3a860c8c70bf215f
Petteri Hellman <Petteri.Hellman@gmail.com>
2023-02-21 13:20:11 +0200

    2122023

--- index.js
+++ index.js
@@ -11,58 +11,6 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
@@ -95,6 +43,8 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
   })
+
+  console.log('note', note)
 
   note.save().then(savedNote => {
     response.json(savedNote)
